f25aebce7583739e481ec2b0bb6271cb69ba643c
Leslie Castellanos <lesliekate@gmail.com>
2021-10-30 14:03:46 -0700

    Commit a misseed migration

--- db/migrate/20211030204011_add_flower_ref_to_plant.rb
+++ db/migrate/20211030204011_add_flower_ref_to_plant.rb
@@ -1,5 +1,5 @@
 class AddFlowerRefToPlant < ActiveRecord::Migration[6.1]
   def change
-    add_reference :plants, :flower_characteristics, foreign_key: true
+    add_reference :plants, :flower_characteristics, null: false, foreign_key: true
   end
 end
