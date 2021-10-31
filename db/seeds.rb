4dedae16d70a7daa17d1e3d4072656848f31c466
Leslie Castellanos <lesliekate@gmail.com>
2021-10-31 14:21:30 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 Plant.create(name: 'Geranium Vine, Purple', exposure:'sun to shade', moisture:"keep moist", description:"perennial")
-Plant.create(name: 'Azalea', exposure:'sun', moisture:"keep moist", description:"perennial")
+az = Plant.create(name: 'Azalea', exposure:'sun', moisture:"keep moist", description:"perennial")
 Plant.create(name: 'Lemon Verbena', exposure:'sun', moisture:"keep moist", description:"perennial")
+
+area = Area.create(name: 'main garden')
+loc = Location.create(name: 'southeast flowerbed', area: area)
+
+Planting.create(date_planted: '2019-03-03', description: 'perennial', plant: az)
+
+
