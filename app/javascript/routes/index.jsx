1c516f39df10e49d0286ffbffa21b04e722e88ca
Leslie Castellanos <lesliekate@gmail.com>
2021-11-07 12:39:36 -0800

    Update routes

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
 import Areas from '../components/Areas';
+import Plants from '../components/Plants';
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Areas} />
-      <Route path="/plants" exact component={Home} />
+      <Route path="/plants" exact component={Plants} />
     </Switch>
   </Router>
 );
