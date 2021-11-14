269d6b83e08539aafd9921724d2e46e28e311171
Leslie Castellanos <lesliekate@gmail.com>
2021-11-14 14:37:48 -0800

    Remove duplicate directory

--- app/javascript/api/routes.js
+++ app/javascript/api/routes.js
@@ -1,6 +1,6 @@
 const routes = {
-	ADD_PLANT: () => 'api/v1/plants/create',
-	DELETE_PLANT: (id) => `api/v1/plants/${id}`
+  ADD_PLANT: () => 'api/v1/plants/create',
+  DELETE_PLANT: (id) => `api/v1/plants/${id}`
 };
 
 export default routes;
--- app/javascript/api_requests/routes.js
+++
@@ -1,6 +0,0 @@
-const routes = {
-	ADD_PLANT: () => 'api/v1/plants/create',
-	DELETE_PLANT: (id) => `api/v1/plants/${id}`
-};
-
-export default routes;
