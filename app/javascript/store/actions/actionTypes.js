f6d203caefacfa4e0d97989a630321597bbfb1ed
Leslie Castellanos <leskat47@users.noreply.github.com>
2021-11-28 16:15:32 -0800

    Reducer for adding planting success (#6)

--- app/javascript/store/actions/actionTypes.js
+++ app/javascript/store/actions/actionTypes.js
@@ -4,7 +4,8 @@ import keymirror from 'keymirror';
 const actions = keymirror({
   GET_AREAS_SUCCESS: null,
   START_LOAD: null,
-  GET_PLANTS_SUCCESS: null
+  GET_PLANTS_SUCCESS: null,
+  ADD_PLANTING_SUCCESS: null
 });
 
 export default actions;
